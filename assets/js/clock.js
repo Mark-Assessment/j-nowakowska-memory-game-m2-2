b9f8e3ef08befef9df86fb79f5d4152c36748200
juginowakowska <>
2023-05-23 19:10:30 +0000

    basic timer added a minutes/seconds breakdown

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -4,5 +4,10 @@ let time = startingTime * 60;
 const clockEl  = document.getElementByClassName("clock");
 
 function updateClock () {
-    
+
+    const minutes = Math.floor(time/60);
+    let seconds = time % 60 ;
+
+    clockEl.innerHTML  = `${minutes}: ${seconds}`;
+    time--;
 }
