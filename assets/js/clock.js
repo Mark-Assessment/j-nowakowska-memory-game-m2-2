95888cdd633fd7372a51e7d74215457c7145fa74
juginowakowska <>
2023-05-23 20:04:54 +0000

    timer is nor working but not stoping

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -1,13 +1,24 @@
-const startingTime = 1;
+const startingTime = 0.1;
 let time = startingTime * 60;
 
-const clockEl  = document.getElementByClassName("clock");
+const clockEl  = document.getElementById("clock");
+
+setInterval (updateClock, 1000);
 
 function updateClock () {
 
     const minutes = Math.floor(time/60);
     let seconds = time % 60 ;
 
-    clockEl.innerHTML  = `${minutes}: ${seconds}`;
+    if (time < 10){
+    seconds = `0${seconds}`;
+    } 
+
+    clockEl.innerHTML  = `${minutes}:${seconds}`;
     time--;
+
 }
+
+
+
+
